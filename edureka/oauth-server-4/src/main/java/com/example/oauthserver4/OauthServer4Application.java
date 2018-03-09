b92e55c6ae22f2c0efde7200f42fd108f6cb8e36
Hamid <c-hamid.jawaid@charter.com>
2018-03-09 17:20:37 +0530

    Authorization-Server-4 Simplified

---
+++ edureka/oauth-server-4/src/main/java/com/example/oauthserver4/OauthServer4Application.java
@@ -0,0 +1,12 @@
+package com.example.oauthserver4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthServer4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthServer4Application.class, args);
+	}
+}
