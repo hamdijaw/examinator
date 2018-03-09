195d0d97e3fe385a016ccbb754c379676a4620f3
Hamid <c-hamid.jawaid@charter.com>
2018-03-09 16:02:50 +0530

    OAuth2 Authorization-Server

---
+++ edureka/oauth-server-3/src/main/java/com/example/oauthserver3/OauthServer3Application.java
@@ -0,0 +1,12 @@
+package com.example.oauthserver3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthServer3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthServer3Application.class, args);
+	}
+}
